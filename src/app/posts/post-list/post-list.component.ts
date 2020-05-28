2b95f7b239776c081a6198ca8abea37d42fa3b20
Hassan <hassan.abbascs@gmail.com>
2020-05-28 17:01:27 +0300

    removed favorites

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -52,16 +52,6 @@ export class PostListComponent implements OnInit, OnDestroy {
     this.postsService.getPosts(this.postsPerPage, this.currentPage);
   }
 
-  // onFavorite(postId: string) {
-  //   if (this.isFavorite) {
-  //     this.isFavorite = false;
-  //     this.favToolTip = "Add to favorites";
-  //   } else {
-  //     this.isFavorite = true;
-  //     this.favToolTip = "Remove from favorites";
-  //   }
-  // }
-
   ngOnDestroy() {
     this.postsSub.unsubscribe;
   }
